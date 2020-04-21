04011af14d3e40addb04423a1a941dd8d9b7eda1
Seunghwan <hetzer12@icloud.com>
2020-04-21 23:37:28 +0900

    Updates

---
+++ precache-manifest.5c836c9facf91cb486fc0873dc9f39e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e82fdf2355c0b321da2a18e4108e2c3",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "abd238622b8423db6819",
+    "url": "/movie_app/static/css/main.a0f529c5.chunk.css"
+  },
+  {
+    "revision": "e8e5b6bb5ea55158c44c",
+    "url": "/movie_app/static/js/2.83baa668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.83baa668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd238622b8423db6819",
+    "url": "/movie_app/static/js/main.ccf3b052.chunk.js"
+  },
+  {
+    "revision": "77df4e0b724824eca1cd",
+    "url": "/movie_app/static/js/runtime-main.0b3e8895.js"
+  }
+]);
